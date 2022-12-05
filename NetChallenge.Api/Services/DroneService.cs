45f296fa9d8cd2a692ab2ef7a1cca72688860f03
Felipe Passamani <fpassamani@paysign.com>
2022-12-05 16:27:13 -0300

    comments next steps

--- NetChallenge.Api/Services/DroneService.cs
+++ NetChallenge.Api/Services/DroneService.cs
@@ -7,6 +7,7 @@ namespace NetChallenge.Api.Services
     {
         public async Task<Output> CreateAsync(Input request)
         {
+            //I will have to order by desc both lists first
             var totalWeightCapacity = request.DronesAssigned.Select(x => x.MaxWeight).Sum();
             var totalWeightToDeliver = request.ScheduledDestinations.Select(x => x.TotalWeightToDeliver).Sum();
             var numberOfTrips = (double)totalWeightToDeliver / totalWeightCapacity;
@@ -24,30 +25,37 @@ namespace NetChallenge.Api.Services
                 {
                     foreach (var drone in request.DronesAssigned)
                     {
-
                         if (!disctionaryDroneTrips.ContainsKey(drone.Name))
                         {
                             disctionaryDroneTrips.Add(drone.Name, new List<Trip>());
                         }
+
+                        //get the actual List<Trip> from the disctionary and add a tripnumber and location  
                         disctionaryDroneTrips[drone.Name].Add(new Trip()
                         {
                             TripNumber = disctionaryDroneTrips[drone.Name].Count() + 1,
-
                         });
 
                         remaningToDeliver = drone.MaxWeight > remaningToDeliver ? remaningToDeliver : remaningToDeliver - drone.MaxWeight;
 
-                        if (remaningToDeliver != 0)
+                        if (remaningToDeliver == 0)
                             break;
                     }
                 }            
             }
+
+            //to determine the output.TotalCost I will have to get from the dictionary DroneTrips the drone that have the max number of trips, and than, subtract 1 and multiply by 100
+
+            //to determine the output.Distribution I will have to transform disctionaryDroneTrips into a list of Distribution using linq
+
+            //I will have to persist (database or cache) the response so I can access in the GetAsync method, and delete the previous values
 
             return output;
         }
 
         public async Task<Output> GetAsync()
         {
+            // I will have to get the valeu from the persistence (database or cache) 
             return new Output();
         }
     }
