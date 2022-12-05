3638736f997fff2ba74c062ebf0645e0a0cb78e6
Felipe Passamani <fpassamani@paysign.com>
2022-12-05 16:04:31 -0300

    where I stoped

--- NetChallenge.Api/Services/DroneService.cs
+++ NetChallenge.Api/Services/DroneService.cs
@@ -7,7 +7,43 @@ namespace NetChallenge.Api.Services
     {
         public async Task<Output> CreateAsync(Input request)
         {
-            return new Output();
+            var totalWeightCapacity = request.DronesAssigned.Select(x => x.MaxWeight).Sum();
+            var totalWeightToDeliver = request.ScheduledDestinations.Select(x => x.TotalWeightToDeliver).Sum();
+            var numberOfTrips = (double)totalWeightToDeliver / totalWeightCapacity;
+            var output = new Output();
+            var destinations = new List<Distribution>();
+            output.TotalCost = 0;
+
+            var disctionaryDroneTrips = new Dictionary<string, List<Trip>>();
+
+            foreach (var destination in request.ScheduledDestinations)
+            {
+                var remaningToDeliver = destination.TotalWeightToDeliver;
+
+                while (remaningToDeliver != 0)
+                {
+                    foreach (var drone in request.DronesAssigned)
+                    {
+
+                        if (!disctionaryDroneTrips.ContainsKey(drone.Name))
+                        {
+                            disctionaryDroneTrips.Add(drone.Name, new List<Trip>());
+                        }
+                        disctionaryDroneTrips[drone.Name].Add(new Trip()
+                        {
+                            TripNumber = disctionaryDroneTrips[drone.Name].Count() + 1,
+
+                        });
+
+                        remaningToDeliver = drone.MaxWeight > remaningToDeliver ? remaningToDeliver : remaningToDeliver - drone.MaxWeight;
+
+                        if (remaningToDeliver != 0)
+                            break;
+                    }
+                }            
+            }
+
+            return output;
         }
 
         public async Task<Output> GetAsync()
